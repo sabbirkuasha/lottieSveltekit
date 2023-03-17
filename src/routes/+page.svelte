ba7ea17951b41c3008c9bf64d955d2848dfb89d1
sabbirz <sabbir.kuasha@gmail.com>
2023-03-18 00:02:06 +0600

    on click load animation
    1. how to use lottie
    2. How to bind lottie with div element
    3. how to activate animation on button click
    4. play a segment of lottie animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	import lottie from 'lottie-web';
+	import fireworks from '$lib/fireworks.json';
+
+	let lottieElement;
+	let startAnimation;
+	onMount(async () => {
+		startAnimation = lottie.loadAnimation({
+			container: lottieElement,
+			renderer: 'svg',
+			animationData: fireworks,
+			loop: false,
+			autoplay: false
+		});
+		startAnimation.goToAndStop(150, true);
+		// const lastFrame = startAnimation.totalFrames - 1;
+		// startAnimation.goToAndStop(lastFrame, true);
+	});
+	async function buttonClicked() {
+		// startAnimation.goToAndStop(150, true);
+		console.log('Button clicked');
+		// startAnimation.goToAndPlay(1, true);
+
+		// Create a Promise that resolves when the animation completes
+		const animationPromise = new Promise((resolve) => {
+			startAnimation.playSegments([[1, 75]], true);
+			startAnimation.addEventListener('complete', resolve);
+		});
+
+		// Wait for the Promise to resolve before logging the next step
+		await animationPromise;
+
+		console.log('Next step');
+		startAnimation.goToAndStop(150, true);
+		// startAnimation.destroy();
+	}
+</script>
+
+<main>
+	<h1 class=".lottie-element text-3xl font-bold">This is lottie animation tutorial process</h1>
+	<div class="w-1/2 h-96 ring " bind:this={lottieElement}>Hello world</div>
+	<button on:click={buttonClicked} class="btn btn-primary btn-xl w-1/2"> Buy Now </button>
+</main>
